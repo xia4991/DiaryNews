124cdfb65764a47cc2d1b5776c2673f476c53676
xia4991 <xab1994@gmail.com>
2026-04-13 21:05:39 +0100

    feat: configure Vite dev server with HTTPS and ignore local certificate files

--- react-frontend/vite.config.js
+++ react-frontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: '0.0.0.0',
+    https: {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem'),
+    },
     proxy: {
       '/api': 'http://localhost:8000',
     },
